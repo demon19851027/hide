c54db8ec9632aad9c6e012673af036225d3e86ee
zsayru <161034724+zsayru@users.noreply.github.com>
2024-05-09 12:30:32 +0900

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,12 +1,12 @@
 import requests
 
-url = 'https://hidemy.io/ru/demo/'
+url = 'https://hidemn.org/ru/demo/'
 
 if 'Ваша электронная почта' in requests.get(url).text:
     
     email = input('Введите электронную почту для получения тестового периода: ')
 
-    response = requests.post('https://hidemy.io/ru/demo/success/', data={
+    response = requests.post('https://hidemn.org/ru/demo/success/', data={
         "demo_mail": f"{email}"
     })
 
